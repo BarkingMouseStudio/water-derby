fd61159847ee58f1a03f4aa7febbca44944d2ebd
Jim Fleming <jimmy.fleming@gmail.com>
2013-04-28 11:39:08 -0700

    Adding Success prefab and font

---
+++ Assets/Scripts/SuccessBehavior.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class SuccessBehavior : MonoBehaviour {
+
+  private TextMesh textMesh;
+
+  void SetText(string val) {
+    if (textMesh) {
+      textMesh.text = val;
+    }
+  }
+
+  void Awake() {
+    textMesh = GetComponentInChildren<TextMesh>();
+  }
+
+	void Start () {
+    SetText("100!");
+	}
+}
