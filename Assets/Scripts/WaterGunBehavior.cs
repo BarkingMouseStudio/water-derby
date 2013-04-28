521fbf88eba071271f03b05b3ccb577449a12cd9
Danielle Swank <danielle@danielleswank.com>
2013-04-28 09:34:28 -0700

    updated gun behaviour

--- Assets/Scripts/WaterGunBehavior.cs
+++ Assets/Scripts/WaterGunBehavior.cs
@@ -3,29 +3,38 @@ using System.Collections;
 
 public class WaterGunBehavior : MonoBehaviour {
 
-  private Vector3 prevMouse = Vector3.zero;
-  private Vector3 center = Vector3.zero;
-  private float centerSpeed = 1;
-  private float gravity = 0.9f;
-  
-  private Camera camera;
+
+  private const float touchScale = 200;
+  private const float angularDrag = 0.9f;
+
   private Vector3 lookAt;
+  private Vector3 prevLookAt = Vector3.zero;
+  private Vector3 deltaLookAt = Vector3.zero;
+  private Vector3 worldPosition;
+  private Vector3 torque = Vector3.zero;
   
-  SpringJoint springJoint;
-  
+  private new Camera camera;
+    
   public void Awake () {
-    springJoint = GetComponent("Spring Joint") as SpringJoint;  
     camera = Camera.main;
+    worldPosition = transform.position;
   }
 
   public void Update () {
-    //if (Input.GetMouseButtonDown(0)) {
     
-      lookAt = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
-
-      transform.LookAt(lookAt);
-
+    lookAt = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+    deltaLookAt = lookAt - prevLookAt;
+    prevLookAt = lookAt;
+    torque += new Vector3(deltaLookAt.y / Screen.height,
+      deltaLookAt.x / Screen.width, 0) * touchScale;
+  	
+    if (torque.magnitude > Mathf.Epsilon) {
+      transform.RotateAround(worldPosition, Camera.main.transform.up, torque.y);
+      transform.RotateAround(worldPosition, Camera.main.transform.right, torque.x);
+      torque *= angularDrag;
+    }
+  		
+    transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.identity, Time.deltaTime * 2f);
       
-   // }
   }
 }
